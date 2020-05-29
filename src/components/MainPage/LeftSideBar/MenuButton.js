29b798b97482d5dcf45bc6d077e56d9647360313
b13markus <fkklymenko@gmail.com>
2020-05-29 09:51:31 +0300

    Fixed left sidebar menu items

--- src/components/MainPage/LeftSideBar/MenuButton.js
+++ src/components/MainPage/LeftSideBar/MenuButton.js
@@ -6,7 +6,7 @@ function MenuButton({ image }) {
 
   return (
     <div className="menu-button">
-      <IconButton color="primary">
+      <IconButton color="primary" style={{ background: 'linear-gradient(0deg, #637EF5, #637EF5)', boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.04)' }}>
         {image}
       </IconButton>
     </div>
